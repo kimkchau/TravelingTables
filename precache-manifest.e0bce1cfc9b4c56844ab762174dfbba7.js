598c948e39067b4605dc57562cfa036262363f15
Kim C <kimkly.chau@gmail.com>
2020-01-09 16:20:02 -0800

    Updates

---
+++ precache-manifest.e0bce1cfc9b4c56844ab762174dfbba7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e3056093d2ad2428056b1ce7a5b0e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b6d0a4407f13d2f39ca",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b0adfa23d1e738ffe45a",
+    "url": "/static/js/2.401ddd76.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.401ddd76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b6d0a4407f13d2f39ca",
+    "url": "/static/js/main.e037589c.chunk.js"
+  },
+  {
+    "revision": "5bd837ffd8012a8e8f7f",
+    "url": "/static/js/runtime-main.ef6ca78c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
