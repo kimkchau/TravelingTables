36603272d03da356313898a0a8806ec187d4c50e
Kim C <kimkly.chau@gmail.com>
2020-01-10 13:42:37 -0800

    updated to the latest cra

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Traveling Tables</h1>
-    </div>
-  );
+class App() extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Traveling Tables</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
